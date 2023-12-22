2eb149ef8010067cd5465512fa7a1b6a1975e004
Bryan Forbes <bryan@reigndropsfall.net>
2023-12-21 23:24:57 -0600

    Add the ability to set the firmware version string using nix

--- nix/zmk.nix
+++ nix/zmk.nix
@@ -5,6 +5,7 @@
 , shield ? null
 , keymap ? null
 , kconfig ? null
+, fwversion ? null
 }:
 
 
@@ -94,7 +95,9 @@ stdenvNoCC.mkDerivation {
   ] ++
   (lib.optional (shield != null) "-DSHIELD=${shield}") ++
   (lib.optional (keymap != null) "-DKEYMAP_FILE=${keymap}") ++
-  (lib.optional (kconfig != null) "-DCONF_FILE=${kconfig}");
+  (lib.optional (kconfig != null) "-DCONF_FILE=${kconfig}") ++
+  (lib.optional (fwversion != null) "-DCONFIG_BT_DIS_FW_REV=y") ++
+  (lib.optional (fwversion != null) "-DCONFIG_BT_DIS_FW_REV_STR='\"${fwversion}\"'");
 
   nativeBuildInputs = [ cmake ninja python dtc gcc-arm-embedded ];
   buildInputs = [ zephyr ];
